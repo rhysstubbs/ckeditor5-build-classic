8d6bb3b7173f4aa0f444d7c12dd9c92d0ec05c64
Rhys Stubbs <r.stubbs@thinkspain.com>
2020-06-01 18:01:00 +0200

    Enable h4s by default

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -137,6 +137,12 @@ ClassicEditor.defaultConfig = {
 				view: 'h3',
 				title: 'Heading 3',
 				class: 'ck-heading_heading3'
+			},
+			{
+				model: 'heading4',
+				view: 'h4',
+				title: 'Heading 4',
+				class: 'ck-heading_heading4'
 			}
 		]
 	},
